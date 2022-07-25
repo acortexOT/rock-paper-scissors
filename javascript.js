7b29b23decb7446d0a8e4c7a9bf90efa05298c1c
acortexOT <105544604+acortexOT@users.noreply.github.com>
2022-07-25 19:00:59 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
-console.log('Hello World!');
+//create variable const computerSelection to hold random computer choice
+
+//create function getComputerChoice() that randomly selects between 0 and 2
+//function getComputerChoice() uses number selection to pick between rock, paper or scissors
+//function getComputerChoice() puts random selection into variable computerSelection
+
+//create variable let playerSelection()
+
+//create function getPlayerChoice() that takes user input [choice between rock paper or scissors], converts toUpperCase, returns it to result.
+    //inside getPlayerChoice(), playerSelection = result
+
+//PLAY ROUND
+//create function playRound with parameters playerSelection and computerSelection
+//function playRound compares using A CONDITIONAL, holds in local variable const result and provides output to console with who won.
+    //playerSelection === computer Selection
+        //Tie game, you both picked result
+    //playerSelection === 'ROCK' && computerSelection === 'SCISSORS'
+    //playerSelection === 'PAPER' && computerSelection === 'ROCK'
+    //playerSelection === 'SCISSORS' && computerSelection === 'PAPER'
+        //You win! playerSelection beats computerSelection!
+    //computerSelection === 'ROCK' && playerSelection === 'SCISSORS'
+    //computerSelection === 'PAPER' && playerSelection === 'ROCK'
+    //computerSelection === 'SCISSORS' && playerSelection === 'PAPER'
+        //You win! playerSelection beats computerSelection!
+    //default: Oops. Invalid choice. Please select ROCK, PAPER or SCISSORS
+        //getPlayerChoice()
+
+    
