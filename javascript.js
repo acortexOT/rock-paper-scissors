c4819f785e4b7c82d4bccbe0dc8d76136287e4b9
acortexot <acortexot@gmail.com>
2022-07-26 07:38:36 -0300

    Fix cancel bug. Still need proper response for invalid entires.

--- javascript.js
+++ javascript.js
@@ -28,21 +28,28 @@ function compareRound() {                //compares using IF statement, holds in
             computerWinTotal += 1; 
     return alert(`You lose. \rComputer's ${computerSelection} beats your ${playerSelection}. \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);    
         }else {
+            --roundCounter;
     return alert(`Oops. Invalid choice. Please select ROCK, PAPER or SCISSORS. \r\rThe score is still you: ${playerWinTotal} computer: ${computerWinTotal}.`)
     } 
 }
 //PLAY ROUND
 function playRound() {
-    playerSelection = prompt("Round " + roundCounter + ". \r\rMake your move: ROCK, PAPER or SCISSORS", 'type your move here').toUpperCase();   //change to all uppercase to allow for input to be case insensitive.
+    playerSelection = prompt("Round " + roundCounter + ". \r\rMake your move: ROCK, PAPER or SCISSORS", 'type your move here');
+    if (playerSelection === null) {roundCounter = 6} else {
+    playerSelection = playerSelection.toUpperCase();   //change to all uppercase to allow for input to be case insensitive.
     getComputerSelection();
     compareRound();
+    }
 }
 
 //GAME    
 function game() {                                                           //create function game() that runs a for loop
     for (let i = 0; i < 5; ++i) {                                           //for i less than or equal to 5, play round and add i
-        ++roundCounter
-        playRound();                                                        //run playRound()
+        if (roundCounter > 5) {i = 5} else {                                //run playRound()
+        i = roundCounter;
+        ++roundCounter;
+        playRound();
+        }                                                        
     }
     //write game summary in console.
 return console.log(`Final score - You: ${playerWinTotal} Computer: ${computerWinTotal}`);
