29ff57213d76502bd82d8bb1bb4091d946880ee5
acortexot <acortexot@gmail.com>
2022-07-26 06:41:36 -0300

    Clean game() interface Change console.log to alert for each round. Add new lines and gameCounter to prompt. Add return values to functions.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let playerWinTotal = 0
 let computerWinTotal = 0
+let roundCounter = 0
 let playerSelection
 let computerSelection
 
@@ -13,30 +14,26 @@ function getComputerSelection() {                             //create random nu
 //COMPARE ROUND
 function compareRound() {                //compares using IF statement, holds in local variable const result and provides output to console with who won.
     if (playerSelection === computerSelection) {                        //Tie game, you both picked same result and each get a point.
-        console.log(`Tie game, you both picked ${playerSelection}`);
         computerWinTotal += 1;
         playerWinTotal += 1;
-        console.log(`The score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);
+    return alert(`Tie game, you both picked ${playerSelection} \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);
     } else if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||    //player wins
         (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
         (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')) {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
             playerWinTotal += 1; 
-            console.log(`The score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);
+    return alert(`You win! \rYour ${playerSelection} beats computer's ${computerSelection}. \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);
     } else if ((computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||     //computer wins
         (computerSelection === 'PAPER' && playerSelection === 'ROCK') ||
         (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')) {
-            console.log(`You lose. ${computerSelection} beats ${playerSelection}.`);
             computerWinTotal += 1; 
-            console.log(`The score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);    
+    return alert(`You lose. \rComputer's ${computerSelection} beats your ${playerSelection}. \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);    
         }else {
-            console.log(`Oops. Invalid choice. Please select ROCK, PAPER or SCISSORS. The score is still you: ${playerWinTotal} computer: ${computerWinTotal}.`)
+    return alert(`Oops. Invalid choice. Please select ROCK, PAPER or SCISSORS. \r\rThe score is still you: ${playerWinTotal} computer: ${computerWinTotal}.`)
     } 
 }
 //PLAY ROUND
 function playRound() {
-    playerSelection = prompt("Make your move: ROCK, PAPER or SCISSORS", 'type your move here');
-    playerSelection = playerSelection.toUpperCase();   //change to all uppercase to allow for input to be case insensitive.
+    playerSelection = prompt("Round " + roundCounter + ". \r\rMake your move: ROCK, PAPER or SCISSORS", 'type your move here').toUpperCase();   //change to all uppercase to allow for input to be case insensitive.
     getComputerSelection();
     compareRound();
 }
@@ -44,10 +41,11 @@ function playRound() {
 //GAME    
 function game() {                                                           //create function game() that runs a for loop
     for (let i = 0; i < 5; ++i) {                                           //for i less than or equal to 5, play round and add i
+        ++roundCounter
         playRound();                                                        //run playRound()
     }
     //write game summary in console.
-    console.log(`Final score - You: ${playerWinTotal} Computer: ${computerWinTotal}`);
+return console.log(`Final score - You: ${playerWinTotal} Computer: ${computerWinTotal}`);
 }
 
  
