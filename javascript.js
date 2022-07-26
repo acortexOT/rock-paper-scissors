a767fce474f058829e617635be7201db1cb7dfe2
acortexot <acortexot@gmail.com>
2022-07-26 19:50:42 -0300

    Fix bug with typo on final iteration. Create initial values

--- javascript.js
+++ javascript.js
@@ -3,19 +3,22 @@ let computerWinTotal = 0
 let roundCounter = 0
 let playerSelection
 let computerSelection
-
-function getComputerSelection() {                             //create random number and hold in computerSelection
+//create random number and hold in computerSelection
+function getComputerSelection() {                             
     randomNumber = Math.floor(Math.random() * 3);
     (randomNumber === 0) ? computerSelection = 'ROCK' :
     (randomNumber === 1) ? computerSelection = 'PAPER': 
     computerSelection = 'SCISSORS'
 }
-
+//RESET VALUES
+function resetValues() {
+roundCounter = 0;
+playerWinTotal = 0;
+computerWinTotal = 0;
+}
 //COMPARE ROUND
 function compareRound() {                //compares using IF statement, holds in local variable const result and provides output to console with who won.
-    if (playerSelection === computerSelection) {                        //Tie game, you both picked same result and each get a point.
-        computerWinTotal += 1;
-        playerWinTotal += 1;
+    if (playerSelection === computerSelection) {                        //Tie game, you both picked same result and no points.
     return alert(`Tie game, you both picked ${playerSelection} \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);
     } else if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||    //player wins
         (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
@@ -28,8 +31,13 @@ function compareRound() {                //compares using IF statement, holds in
             computerWinTotal += 1; 
     return alert(`You lose. \rComputer's ${computerSelection} beats your ${playerSelection}. \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);    
         }else {
-            --roundCounter;
-    return alert(`Oops. Invalid choice. Please select ROCK, PAPER or SCISSORS. \r\rThe score is still you: ${playerWinTotal} computer: ${computerWinTotal}.`)
+            if(roundCounter === 5) {
+                alert(`Oops. Invalid choice. Please select ROCK, PAPER or SCISSORS. \r\rThe score is still you: ${playerWinTotal} computer: ${computerWinTotal}.`);
+                playRound();
+            }else {
+                --roundCounter;
+                return alert(`Oops. Invalid choice. Please select ROCK, PAPER or SCISSORS. \r\rThe score is still you: ${playerWinTotal} computer: ${computerWinTotal}.`);
+            }
     } 
 }
 //PLAY ROUND
@@ -41,11 +49,12 @@ function playRound() {
     compareRound();
     }
 }
-
 //GAME    
 function game() {                                                           //create function game() that runs a for loop
+    resetValues();
     for (let i = 0; i < 5; ++i) {                                           //for i less than or equal to 5, play round and add i
-        if (roundCounter > 5) {i = 5} else {                                //run playRound()
+        if (roundCounter > 5) {i = 5;}
+        else {                                                              //run playRound()
         i = roundCounter;
         ++roundCounter;
         playRound();
