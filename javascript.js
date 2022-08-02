4839792f4448f2ce91b71fef0f9bc63ffe0b8fb9
acortexot <acortexot@gmail.com>
2022-08-02 20:41:47 -0300

    First attempt at code for UI

--- javascript.js
+++ javascript.js
@@ -19,49 +19,45 @@ computerWinTotal = 0;
 //COMPARE ROUND
 function compareRound() {                //compares using IF statement, holds in local variable const result and provides output to console with who won.
     if (playerSelection === computerSelection) {                        //Tie game, you both picked same result and no points.
-    return alert(`Tie game, you both picked ${playerSelection} \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);
+        return results.textContent = `Tie game, you both picked ${playerSelection} \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`;
     } else if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||    //player wins
         (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
         (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')) {
             playerWinTotal += 1; 
-    return alert(`You win! \rYour ${playerSelection} beats computer's ${computerSelection}. \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);
+        return results.textContent = `You win! \rYour ${playerSelection} beats computer's ${computerSelection}. \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`;
     } else if ((computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||     //computer wins
         (computerSelection === 'PAPER' && playerSelection === 'ROCK') ||
         (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')) {
             computerWinTotal += 1; 
-    return alert(`You lose. \rComputer's ${computerSelection} beats your ${playerSelection}. \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`);    
-        }else {
-            if(roundCounter === 5) {
-                alert(`Oops. Invalid choice. Please select ROCK, PAPER or SCISSORS. \r\rThe score is still you: ${playerWinTotal} computer: ${computerWinTotal}.`);
-                playRound();
-            }else {
-                --roundCounter;
-                return alert(`Oops. Invalid choice. Please select ROCK, PAPER or SCISSORS. \r\rThe score is still you: ${playerWinTotal} computer: ${computerWinTotal}.`);
-            }
-    } 
-}
-//PLAY ROUND
-function playRound() {
-    playerSelection = prompt("Round " + roundCounter + ". \r\rMake your move: ROCK, PAPER or SCISSORS", 'type your move here');
-    if (playerSelection === null) {roundCounter = 6} else {
-    playerSelection = playerSelection.toUpperCase();   //change to all uppercase to allow for input to be case insensitive.
-    getComputerSelection();
-    compareRound();
+        return results.textContent = `You lose. \rComputer's ${computerSelection} beats your ${playerSelection}. \r\rThe score is now you: ${playerWinTotal}. Computer: ${computerWinTotal}.`;    
+    }else {
+        return results.textContent = `Oops. Something went wrong. Please select ROCK, PAPER or SCISSORS. \r\rThe score is still you: ${playerWinTotal} computer: ${computerWinTotal}.`;
     }
-}
+} 
 //GAME    
 function game() {                                                           //create function game() that runs a for loop
-    resetValues();
-    for (let i = 0; i < 5; ++i) {                                           //for i less than or equal to 5, play round and add i
-        if (roundCounter > 5) {i = 5;}
-        else {                                                              //run playRound()
-        i = roundCounter;
-        ++roundCounter;
-        playRound();
-        }                                                        
-    }
+        if (playerWinTotal === 5 || computerWinTotal === 5) {resetValues();};
+        getComputerSelection();
+        compareRound();                                                     
     //write game summary in console.
-return console.log(`Final score - You: ${playerWinTotal} Computer: ${computerWinTotal}`);
+return results.textContent = `Final score - You: ${playerWinTotal} Computer: ${computerWinTotal}`;
 }
+//PLAYER SELECTION
+const container = document.querySelector('#container');
 
+const playerDiv = document.querySelector('.player-score');
+playerDiv.textContent = playerWinTotal;
+
+const computerDiv = document.querySelector('.computer-score');
+computerDiv.textContent = computerWinTotal;
+
+const results = document.querySelector('.results');
+
+const button = document.querySelectorAll('button');
+button.forEach(btn => btn.addEventListener('click', getPlayerSelection));
+
+function getPlayerSelection(e) {
+    playerSelection = e.target.attributes.id.value;
+    game();
+}
  
