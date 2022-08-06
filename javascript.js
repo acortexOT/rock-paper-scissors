85813b63b4ff176860bdb8aebff818adb0c8fb86
acortexot <acortexot@gmail.com>
2022-08-06 16:00:21 -0300

    Fix transition images

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ function getComputerSelection() {
 }
 
 //SHOW IMAGES OF SELECTIONS
-function showImages(){
+function showSelectionImages(){
     const playerimg = document.querySelector('.playerimg');
     const pImgSelected = document.createElement('img');
     pImgSelected.src = `images/${playerSelection}.png`;
@@ -61,13 +61,15 @@ function compareRound() {                //compares using IF statement, holds in
 //GAME    
 function game() {                                                           //create function game() that runs a for loop
     getComputerSelection();
-    showImages();
+    showSelectionImages();
     compareRound();
     playerDiv.textContent = playerWinTotal;
-    computerDiv.textContent = computerWinTotal;                                                     
+    computerDiv.textContent = computerWinTotal;   
+    setTransitionImage();                                                  
     if (playerWinTotal === 5 || computerWinTotal === 5) {
         results.textContent +=`Final score - You: ${playerWinTotal} Computer: ${computerWinTotal}`;
         resetValues();
+        setNewGameImage();
     };
 }
 
@@ -83,6 +85,26 @@ const results = document.querySelector('.results');
 
 const button = document.querySelectorAll('button');
 button.forEach(btn => btn.addEventListener('click', getPlayerSelection));
+
+setNewGameImage(); //places opening image under header
+
+function setNewGameImage() {
+    const transitionDiv = document.querySelector('.transition');
+    const newGame = document.createElement('img');
+    newGame.id = '#newGame';
+    newGame.src = 'images/newGame.png';
+    if (transitionDiv.firstChild) { //removes previous image if not first game
+        transitionDiv.removeChild(transitionDiv.firstChild);
+    }
+    transitionDiv.appendChild(newGame); 
+}
+
+function setTransitionImage() {
+    const transitionDiv = document.querySelector('.transition');
+    transitionDiv.removeChild(transitionDiv.firstChild);
+    const vsImage = document.createElement('img');
+    vsImage.id = '#vs';
+    vsImage.src = 'images/vs.jpg';
+    transitionDiv.appendChild(vsImage);
+} 
  
-//embed link for loading gif
-//<iframe src="https://giphy.com/embed/Pk2qQx7aqx4TVc3ZGT" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/MetroByTMobile-countdown-metro-by-t-mobile-rule-your-day-Pk2qQx7aqx4TVc3ZGT">via GIPHY</a></p>
